bf1b3a0f3c477ebdb46e7607c94b9b126394225e
adaptedbee <adaptedbee@yandex.ru>
2017-03-02 14:34:14 +0300

    12. Updating State with Reducers

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -23,7 +23,7 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button className="likes">&hearts;&nbsp;{post.likes}</button>
+            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts;&nbsp;{post.likes}</button>
             <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>&nbsp;
